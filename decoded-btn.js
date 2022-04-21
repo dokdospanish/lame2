47f1e15a03bd783bab3b79b3d052060b7dbbb27b
dokdospanish <67682780+dokdospanish@users.noreply.github.com>
2022-04-21 17:27:00 +0900

    Update decoded-btn.js

--- decoded-btn.js
+++ decoded-btn.js
@@ -1,37 +1,42 @@
-/* SETTINGS */
-LAME_SUS_LIMIT = 85;
-LAME_DIFF_UNIT = (typeof setting == 'undefined' || setting == 'word') ? 'char' : 'word'; /* for objection similarity */
-
-/* LINKS */
+LAME_CDN = `https://lame.netlify.com/`;
 LAME_CURRENT_URL = window.location.href;
-LAME_CDN = 'http://lamer.netlify.app/';
 
-/* REGEX */
-LAME_SUPPORTED_PAGES = {
-  'project-page': ['project-page.js', /lame\.netlify\.app/],
-  'crowd-monitoring': ['crowd-monitoring.js', /a\.flit\.to:4435\/admin#\/admin\/req_tr/],
-  'arcade-objection': ['arcade-objection.js', /a3\.flit\.to\/#\/arcade\/arcade-objection/],
-  'arcade-history': ['arcade-history.js', /a3\.flit\.to\/#\/arcade\/arcade-user-history/],
-  'pro-applicant': ['pro-applicant.js', /a3\.flit\.to\/#\/pro-tr\/pro-applicant\/\d/],
-  'flitto-api': ['mt-similarity.js', /api-demo\.flit\.to/],
-  'translators-to': ['mt-similarity.js', /translators\.to/]
+
+let LAME_SUPPORTED_SITES = [
+  {rx: /lame\.netlify\.app/, scriptSrc: `${LAME_CDN}project=page.js`},
+  {rx: /a\.flit\.to:4435\/admin#\/admin\/req_tr/, scriptSrc: `${LAME_CDN}crowd-monitoring.js`},
+  {rx: /a3\.flit\.to\/#\/arcade\/arcade-objection/, scriptSrc: `${LAME_CDN}arcade-objection.js`},
+  {rx: /a3\.flit\.to\/#\/arcade\/arcade-user-history/, scriptSrc: `${LAME_CDN}arcade-history.js`},
+  {rx: /api-demo\.flit\.to/, scriptSrc: `${LAME_CDN}extend-simi-sites.js`},
+  {rx: /translators\.to/, scriptSrc: `${LAME_CDN}extend-simi-sites.js`},
+  {rx: /a3\.flit\.to\/#\/pro-tr\/pro-applicant\/\d/, scriptSrc: `${LAME_CDN}pro-applicant.js`},
+];
+
+
+let matchedScript;
+for (let {rx, scriptSrc} of LAME_SUPPORTED_SITES) {
+  if (rx.test(LAME_CURRENT_URL)) { matchedSite = scriptSrc }
+}
+if (!matchedScript) { throw 'Site not supported by LAME' }
+
+
+
+if (!document.head.querySelectorAll(`[src="${matchedScript}"]`).length) {
+  injectRemoteScript(matchedScript)
+  .then(lameify())
+}
+else {
+  lameify()
 }
 
-/* FLOW */
-for (let page of LAME_SUPPORTED_PAGES) {
-  /* If regex matches and script not yet injected */
-  if (page[1].test(LAME_CURRENT_URL) && !document.head.querySelectorAll(`[src="${LAME_CDN}${page[0]}"]`).length) {
-    injectRemoteScript(`${LAME_CDN}${page[0]}"]`)
-  } else { reclick() }
-}
 
-/* EXTRACTED */
+
 function injectRemoteScript(src) {
   return new Promise((resolve, reject) => {
-      const script = document.createElement('script');
-      script.src = src;
-      script.addEventListener('load', resolve);
-      script.addEventListener('error', e => reject(e.error));
-      document.head.appendChild(script);
+    let script = document.createElement('script');
+    script.src = src;
+    script.addEventListener('load', resolve);
+    script.addEventListener('error', e => reject(e.error));
+    document.head.appendChild(script);
   });
 }
